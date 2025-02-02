2558800236613973548fb9cb8b6b32940415c067
AshfaqSourav <ashfaqahmed3339@gmail.com>
2025-02-03 01:26:08 +0600

    Platform windows updated

--- src/main/java/locators/androidLocators/LoginPageLocatorsAndroid.java
+++ src/main/java/locators/androidLocators/LoginPageLocatorsAndroid.java
@@ -1,9 +1,8 @@
 package locators.androidLocators;
 
 public class LoginPageLocatorsAndroid {
-    public static final String FALLBACK_BUTTON = "new UiSelector().className(\"android.widget.Button\").index(0)";
-    public static final String LOGIN_WITH_EMAIL_BUTTON = "new UiSelector().className(\"android.widget.ImageView\").index(4)";
-    public static final String LOGIN_EMAIL_TEXT = "new UiSelector().className(\"android.widget.EditText\")";
+    public static final String LOGIN_WITH_EMAIL_BUTTON = "//android.widget.ImageView[@content-desc=\"Continue with Email\" or @content-desc=\"Lanjutkan dengan Email\"]";
+    public static final String LOGIN_EMAIL_TEXT = "//android.widget.EditText";
     public static final String LOGIN_EMAIL_NEXT_BUTTON = "//android.widget.Button[@content-desc=\"Lanjutkan\" or @content-desc=\"Next\"]";
     public static final String LOGIN_EMAIL_OTP_2 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[2]";
     public static final String LOGIN_EMAIL_OTP_3 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[3]";
@@ -11,11 +10,11 @@ public class LoginPageLocatorsAndroid {
     public static final String LOGIN_EMAIL_OTP_5 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[5]";
     public static final String LOGIN_EMAIL_OTP_1 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[1]";
     public static final String LOGIN_EMAIL_OTP_6 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[6]";
-    public static final String LOGIN_EMAIL_VERIFY_BUTTON = "new UiSelector().className(\"android.widget.Button\").index(5)";
-    public static final String TURN_ON_NOTIFICATION_BUTTON = "new UiSelector().className(\"android.widget.Button\").index(3)";
+    public static final String LOGIN_EMAIL_VERIFY_BUTTON = "//android.widget.Button[@content-desc=\"Verifikasi\" or @content-desc=\"Verify\"]";
+    public static final String TURN_ON_NOTIFICATION_BUTTON = "//android.widget.Button[@content-desc=\"Nyalakan notifikasi\" or @content-desc=\"Turn on notifications\"]";
     public static final String ALLOW_NOTIFICATION = "com.android.permissioncontroller:id/permission_allow_button";
     public static final String DONT_ALLOW_NOTIFICATION = "com.android.permissioncontroller:id/permission_deny_button";
-    public static final String MAYBE_LATER = "new UiSelector().className(\"android.widget.Button\").index(4)";
+    public static final String MAYBE_LATER = "//android.widget.Button[@content-desc=\"Mungkin Nanti\" or @content-desc=\"Maybe Later\"]";
 
 }
 
