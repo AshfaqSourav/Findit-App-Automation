e29025bab7938698a49a6b64d42f22c93c18ceeb
AshfaqSourav <ashfaqahmed3339@gmail.com>
2025-02-18 21:02:49 +0600

    Removed LFS tracking from all files

--- src/main/java/locators/androidLocators/LogoutPageLocatorsAndroid.java
+++
@@ -1,8 +0,0 @@
-package locators.androidLocators;
-
-public class LogoutPageLocatorsAndroid {
-    public static final String PROFILE_BUTTON = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[7]";
-    public static final String SETTINGS_BUTTON = "//android.widget.ScrollView/android.widget.ImageView[1]";
-    public static final String LOGOUT_BUTTON = "//android.widget.ImageView[@content-desc=\"Keluar\" or @content-desc=\"Logout\"]";
-    public static final String LOGIN_POPUP = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View";
-}
