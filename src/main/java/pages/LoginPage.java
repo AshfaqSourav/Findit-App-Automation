f472a2ca4729eabdb11395b2e35a14a8897af576
AshfaqSourav <ashfaqahmed3339@gmail.com>
2025-02-18 17:00:48 +0600

    Cicd locator test 11

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -19,7 +19,7 @@ public class LoginPage extends BasePage {
         System.out.println("visible continue with email");
         this.clickButton("LOGIN_WITH_EMAIL_BUTTON", LocatorClassName.LOGIN_LOCATOR_CLASS.toString(), LocatorType.XPATH);
         System.out.println("Clicked continue with email");
-        waitForVisibilityOfElement("LOGIN_EMAIL_TEXT", LocatorClassName.LOGIN_LOCATOR_CLASS.toString(), LocatorType.XPATH,20);
+        waitForVisibilityOfElement("LOGIN_EMAIL_TEXT", LocatorClassName.LOGIN_LOCATOR_CLASS.toString(), LocatorType.XPATH,60);
         System.out.println("visible txt");
         this.clickTextField("LOGIN_EMAIL_TEXT", LocatorClassName.LOGIN_LOCATOR_CLASS.toString(), LocatorType.XPATH, email);
         System.out.println("Email Entered");
