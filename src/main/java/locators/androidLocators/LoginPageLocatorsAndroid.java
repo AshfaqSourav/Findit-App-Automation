2a3e33949d5a9fa82993032d139bde9397998c9d
AshfaqAhmed <ashfaq.ahmed3339@gmail.com>
2025-04-21 19:35:44 +0600

    Added loginTest cases and configured device config

--- src/main/java/locators/androidLocators/LoginPageLocatorsAndroid.java
+++ src/main/java/locators/androidLocators/LoginPageLocatorsAndroid.java
@@ -4,17 +4,21 @@ public class LoginPageLocatorsAndroid {
     public static final String LOGIN_WITH_EMAIL_BUTTON = "//android.widget.ImageView[@content-desc=\"Continue with Email\" or @content-desc=\"Lanjutkan dengan Email\"]";
     public static final String LOGIN_EMAIL_TEXT = "//android.widget.EditText";
     public static final String LOGIN_EMAIL_NEXT_BUTTON = "//android.widget.Button[@content-desc=\"Lanjutkan\" or @content-desc=\"Next\"]";
+    public static final String LOGIN_EMAIL_OTP_1 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[1]";
     public static final String LOGIN_EMAIL_OTP_2 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[2]";
     public static final String LOGIN_EMAIL_OTP_3 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[3]";
     public static final String LOGIN_EMAIL_OTP_4 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[4]";
     public static final String LOGIN_EMAIL_OTP_5 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[5]";
-    public static final String LOGIN_EMAIL_OTP_1 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[1]";
     public static final String LOGIN_EMAIL_OTP_6 = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]/android.widget.EditText[6]";
     public static final String LOGIN_EMAIL_VERIFY_BUTTON = "//android.widget.Button[@content-desc=\"Verifikasi\" or @content-desc=\"Verify\"]";
     public static final String TURN_ON_NOTIFICATION_BUTTON = "//android.widget.Button[@content-desc=\"Nyalakan notifikasi\" or @content-desc=\"Turn on notifications\"]";
     public static final String ALLOW_NOTIFICATION = "com.android.permissioncontroller:id/permission_allow_button";
     public static final String DONT_ALLOW_NOTIFICATION = "com.android.permissioncontroller:id/permission_deny_button";
     public static final String MAYBE_LATER = "//android.widget.Button[@content-desc=\"Mungkin Nanti\" or @content-desc=\"Maybe Later\"]";
-
+    public static final String EMAIL_EMPTY_INVALID_MESSAGE = "(//android.view.View[@content-desc=\"Masukkan email Anda\" or @content-desc=\"Enter your email\"])[2]";
+    public static final String EMAIL_INVALID_INPUT_MESSAGE = "//android.view.View[@content-desc=\"Masukkan email yang valid\" or @content-desc=\"Enter a valid email\"]";
+    public static final String EMAIL_INVALID_INPUT_MESSAGE2 = "//android.view.View[@content-desc=\"email: Masukkan alamat email dengan format yang benar.\" or @content-desc=\"email: Enter a valid email address.\"]";
+    public static final String OTP_INVALID_MESSAGE = "(//android.view.View[@content-desc=\"Masukkan 6 digit OTP\" or @content-desc=\"Enter 6 digit OTP\"])[2]";
+    public static final String OTP_INVALID_MESSAGE2 = "//android.view.View[@content-desc=\"Kode OTP salah atau kedaluwarsa\" or @content-desc=\"OTP code is wrong or expired\"]";
 }
 
--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -6,10 +6,53 @@ import pages.BasePage;
 
 
 public class LoginPageTest extends BaseTest {
-    @Test(groups = {"smoke"})
+    @Test(priority =1,groups = {"smoke"})
     @Description("Verify login of a user")
     public void testLoginWithEmail() throws Exception {
     	loginPage.LoginWithEmail(this.buyerEmail);
+        BasePage.quitDriver();
+    }
+
+    @Test(groups = {"regression"})
+    @Description("Verify Email Input validation")
+    public void testEmailInputValidations() throws Exception {
+        loginPage.emailInputEmpty();
+        BasePage.quitDriver();
+    }
+    @Test(groups = {"regression"})
+    @Description("Verify Email Input validation")
+    public void testEmailInvalidInput1() throws Exception {
+        loginPage.emailInputInvalidText();
+        BasePage.quitDriver();
+    }
+    @Test(groups = {"regression"})
+    @Description("Verify Email Input validation with 'abc@gmail'")
+    public void testEmailInvalidInput2() throws Exception {
+        loginPage.emailInputInvalidText2();
+        BasePage.quitDriver();
+    }
+    @Test(groups = {"regression"})
+    @Description("Verify Email Input validation with ' '")
+    public void testEmailInvalidInput3() throws Exception {
+        loginPage.emailInputInvalidText3();
+        BasePage.quitDriver();
+    }
+    @Test(groups = {"regression"})
+    @Description("Verify OTP empty input")
+    public void testOtpInputEmpty() throws Exception {
+        loginPage.otpInputEmpty(this.sellerEmail);
+        BasePage.quitDriver();
+    }
+    @Test(groups = {"regression"})
+    @Description("Verify OTP empty input")
+    public void testOtpInvalidInput() throws Exception {
+        loginPage.otpInputInvalid(this.sellerEmail);
+        BasePage.quitDriver();
+    }
+    @Test(groups = {"regression"})
+    @Description("Verify OTP empty input")
+    public void testOtpInvalidInput2() throws Exception {
+        loginPage.otpInvalidInput2(this.sellerEmail);
         BasePage.quitDriver();
     }
 
